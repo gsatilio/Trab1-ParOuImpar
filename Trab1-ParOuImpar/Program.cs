67e6f2489969954f96dd65cc28e49fb6d9c618d1
Guilherme <Guilherme@DESKTOP-V4NM6ON>
2024-04-17 17:01:02 -0300

    Add project files.

---
+++ Trab1-ParOuImpar/Program.cs
@@ -0,0 +1,19 @@
+﻿// 1 - faça um algoritmo que leia um numero e informe se ele é par ou impar
+
+
+float numero, resto;
+
+Console.WriteLine("Por favor, informe um numero.");
+numero = float.Parse(Console.ReadLine());
+
+resto = (numero % 2);
+
+if(resto == 0)
+{
+    Console.WriteLine("O numero eh par!");
+} else
+{
+    Console.WriteLine("O numero eh impar!");
+}
+
+Console.ReadKey();
